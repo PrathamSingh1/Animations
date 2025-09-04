b23d4d5a6732f0dc54b63a66907ed59ecb386605
PrathamSingh1 <prathamjii36@gmail.com>
2025-09-04 08:51:27 +0000

    SVG Animation

--- SVG Animations/script.js
+++ SVG Animations/script.js
@@ -1,1 +1,25 @@
-var initialPath = `M 10 100 Q 250 100 490 100`
+var initialPath = `M 10 100 Q 500 100 990 100`
+
+var finalPath = `M 10 100 Q 500 100 990 100`
+
+var string = document.querySelector("#string")
+
+string.addEventListener("mousemove", function(dets) {
+    initialPath = `M 10 100 Q ${dets.x} ${dets.y} 990 100`
+    console.log(initialPath);
+
+    gsap.to("svg path", {
+        attr: {d: initialPath},
+        duration: 0.2,
+        ease: "power3.out"
+    })
+})
+
+
+string.addEventListener("mouseleave", function() {
+    gsap.to("svg path", {
+        attr: {d: finalPath},
+        duration: 0.8,
+        ease: "elastic.out(1,0.2)"
+    })
+})
